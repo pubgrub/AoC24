e08553b3b410f10153e1473f104cc4ab43b01685
Michael Fuchs <pubgrub@gmx.de>
2024-12-03 14:42:46 +0100

    day 03 done

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -12,13 +12,38 @@ fn main() {
 
 fn solve1(lines: &Vec<String>) -> i32 {
     let mut result: i32 = 0;
-    let re = Regex::new(r"(mul\(\d+,\d+\))").unwrap();
-    for line in lines {}
-
+    let re1 = Regex::new(r"(mul\(\d+,\d+\))").unwrap();
+    let re2 = Regex::new(r"(\d+)").unwrap();
+    for line in lines {
+        let muls: Vec<&str> = re1.find_iter(line).map(|s| s.as_str()).collect();
+        for mul in muls {
+            let factors: Vec<&str> = re2.find_iter(mul).map(|s| s.as_str()).collect();
+            result += factors[0].parse::<i32>().unwrap() * factors[1].parse::<i32>().unwrap();
+        }
+    }
     result
 }
 
 fn solve2(lines: &Vec<String>) -> i32 {
     let mut result: i32 = 0;
+    let mut enabled = true;
+    let re1 = Regex::new(r"(mul\(\d+,\d+\))|(do\(\))|(don't\(\))").unwrap();
+    let re2 = Regex::new(r"(\d+)").unwrap();
+    for line in lines {
+        let instructions: Vec<&str> = re1.find_iter(line).map(|s| s.as_str()).collect();
+        for instr in instructions {
+            match instr {
+                "do()" => enabled = true,
+                "don't()" => enabled = false,
+                _ => {
+                    if enabled {
+                        let factors: Vec<&str> = re2.find_iter(instr).map(|s| s.as_str()).collect();
+                        result +=
+                            factors[0].parse::<i32>().unwrap() * factors[1].parse::<i32>().unwrap();
+                    }
+                }
+            }
+        }
+    }
     result
 }
