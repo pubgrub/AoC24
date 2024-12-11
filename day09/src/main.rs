7d910532db75b0eaac390c008d79f7bd272b697d
Michael Fuchs <fox@fuechse.de>
2024-12-11 17:03:18 +0100

    day 10 done

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,7 @@
 use file;
+use std::{collections::HashMap, hash::Hash};
 
-const TEST: i32 = 0;
+const TEST: i32 = 1;
 
 fn main() {
     let day = env!("CARGO_PKG_NAME");
@@ -9,12 +10,92 @@ fn main() {
     println!("Part 2: {}", solve2(&lines));
 }
 
-fn solve1(lines: &Vec<String>) -> i32 {
-    let mut result: i32 = 0;
+fn solve1(lines: &Vec<String>) -> usize {
+    let mut result: usize = 0;
+    let dmap:Vec<usize> = lines[0].chars().map(|s| s.to_digit(10).unwrap() as usize ).collect();
+    let mut l_pointer = 0;
+    let mut r_pointer = dmap.len() -1;
+    let mut d_pointer = 0;
+    let mut id = 0;
+    let mut blocks;
+    let mut free_space:usize = 0;
+    let mut blocks_to_move;
+    let mut res_str = "".to_string();
+    if r_pointer % 2 == 1 {
+        r_pointer -= 1;
+    }
+    let mut remaining_blocks = dmap[r_pointer];
+
+    while r_pointer >= l_pointer {  
+        if l_pointer % 2 == 0 {
+            id = l_pointer / 2;
+            if r_pointer > l_pointer {
+                blocks = dmap[l_pointer];
+            } else {
+                blocks = remaining_blocks;
+            }
+            result += id * blocks * (blocks-1) / 2 +id *  d_pointer * blocks; 
+            res_str.push_str( &id.to_string().repeat(blocks));
+            d_pointer += blocks;
+            l_pointer +=1;
+            free_space = dmap[l_pointer];
+        } else {
+            id = r_pointer / 2;            
+            //while free_space > 0 && r_pointer > l_pointer {
+
+            while free_space > 0 && r_pointer > l_pointer{
+                blocks_to_move = free_space.min(remaining_blocks);
+                free_space -= blocks_to_move;
+                remaining_blocks -= blocks_to_move;
+                result += id * blocks_to_move  * (blocks_to_move-1) / 2 + id * d_pointer * blocks_to_move ; 
+                res_str.push_str( &id.to_string().repeat(blocks_to_move));
+                d_pointer += blocks_to_move;
+                if remaining_blocks == 0 {
+                    r_pointer -= 2;
+                    remaining_blocks = dmap[r_pointer];
+                    id = r_pointer / 2;
+                }
+            }
+            l_pointer +=1;
+
+        }
+
+    }
+
+    println!("res_str: {}", res_str);
     result
 }
 
 fn solve2(lines: &Vec<String>) -> i32 {
     let mut result: i32 = 0;
+    let mut gaps:HashMap<usize,Vec<usize>> = HashMap::new();
+    let mut max_gap:usize=0;
+    let dmap:Vec<usize> = lines[0].chars().map(|s| s.to_digit(10).unwrap() as usize ).collect();
+    let mut d_pointer = 0;
+    let mut file_size;
+    let mut files:HashMap<usize,(usize, usize)> = HashMap::new(); // id: (pos,size)
+    for i in 0..dmap.len(){
+        if i % 2 == 1 {
+            gaps.entry(dmap[i]).or_insert(vec![]) .push(d_pointer);
+        } else {
+            files.insert(i / 2, (d_pointer, dmap[i]));
+        }
+        max_gap = *gaps.keys().max().unwrap();
+        d_pointer += dmap[i];
+    }
+    println!("{:?}", gaps);
+    for i in (0..dmap.len()).step_by(2).rev() {
+        let id = i / 2;
+        file_size = dmap[i];
+        for gap_size in file_size..=max_gap {
+            if gaps.contains_key(&gap_size) {
+                
+                files.insert(id,(gaps.get_mut(&id).unwrap().remove(0),dmap[i]));
+                break;
+            }
+        }
+
+    }
+
     result
 }
---
+++ day10/src/main.rs
@@ -0,0 +1,86 @@
+use file;
+
+const TEST:i32 = 0;
+
+fn main() {
+    let day = env!("CARGO_PKG_NAME");
+    let lines = file::read_file(day, TEST);
+    println!("Part 1: {}",solve1(&lines));
+    println!("Part 2: {}",solve2(&lines));
+}
+
+fn solve1(lines:&Vec<String>) -> i32 {
+    let mut result:i32 = 0;
+
+    let width = lines[0].len() + 2;
+    let offs:Vec<i32> = vec![-(width as i32), 1, width as i32, -1];
+    let mut levels:Vec<i32> = vec![-1;width];
+    for line in lines {
+        levels.push(-1);
+        levels.append(&mut line.chars().map(|c| c.to_digit(10).unwrap() as i32).collect());
+        levels.push(-1);
+    }
+    levels.append(&mut vec![-1;width]);
+
+    for i in 0..levels.len() {
+        if levels[i] == 0 {
+            let mut nines:Vec<usize> = vec![];
+            result += climb1(i, &levels, &offs, nines).len() as i32;
+        }
+    }
+    result
+}
+
+fn climb1
+(pos:usize, levels:&Vec<i32>, offs:&Vec<i32>,mut nines:Vec<usize>) -> Vec<usize>{
+    let level = levels[pos];
+    if level ==9 {
+        if !nines.contains(&pos){
+            nines.push(pos);
+        }
+        return nines;
+    }
+    for dir in offs {
+        let new_pos = (pos as i32 + dir) as usize;
+        if levels[new_pos] == level + 1 {
+            nines = climb1(new_pos, levels, offs, nines);
+        }
+    }
+nines
+}
+
+fn solve2(lines:&Vec<String>) -> i32 {
+    let mut result:i32 = 0;
+
+    let width = lines[0].len() + 2;
+    let offs:Vec<i32> = vec![-(width as i32), 1, width as i32, -1];
+    let mut levels:Vec<i32> = vec![-1;width];
+    for line in lines {
+        levels.push(-1);
+        levels.append(&mut line.chars().map(|c| c.to_digit(10).unwrap() as i32).collect());
+        levels.push(-1);
+    }
+    levels.append(&mut vec![-1;width]);
+
+    for i in 0..levels.len() {
+        if levels[i] == 0 {
+            let mut nines:i32= 0;
+            result += climb2(i, &levels, &offs, nines);
+        }
+    }
+    result
+}
+fn climb2
+(pos:usize, levels:&Vec<i32>, offs:&Vec<i32>,mut nines:i32) -> i32{
+    let level = levels[pos];
+    if level ==9 {
+        return nines + 1;
+    }
+    for dir in offs {
+        let new_pos = (pos as i32 + dir) as usize;
+        if levels[new_pos] == level + 1 {
+            nines = climb2(new_pos, levels, offs, nines);
+        }
+    }
+nines
+}
