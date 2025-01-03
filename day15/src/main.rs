85ba34ed2caced1d939ebed961f50456ce1d3c41
Michael Fuchs <pubgrub@gmx.de>
2025-01-04 00:35:03 +0100

    15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -109,6 +109,7 @@ fn solve2(lines: &Vec<String>) -> i32 {
         if l.len() == 0 {
             is_map = false;
             cursor = map.iter().position(|i| i == &4usize).unwrap() as i32;
+            print_map(' ', &map, width);
             continue;
         }
         if is_map {
@@ -129,8 +130,8 @@ fn solve2(lines: &Vec<String>) -> i32 {
                             0 | 1 => {
                                 // O
                                 if move_cursor == false {
-                                    move_block = true;
                                     move_cursor = true;
+                                    move_block = true;
                                     next_cursor = next_pos;
                                 }
                             }
@@ -153,49 +154,60 @@ fn solve2(lines: &Vec<String>) -> i32 {
                             _ => {}
                         }
                     }
+                    print_map(c, &map, width);
                 } else {
                     // up-down
-                    let mut do_move:bool;
-                    let mut tiles_to_move:Vec<i32> = vec![];
-                    let mut ret_tiles_to_move:Vec<i32>;
+                    let tiles_to_move: Vec<i32> = vec![];
                     let (do_move, mut ret_tiles_to_move) = walk(&map, tiles_to_move, cursor, off);
                     if do_move == true {
                         ret_tiles_to_move.reverse();
                         for t in &ret_tiles_to_move {
-                            map[*t as usize + off as usize] = map[*t as usize];
+                            println!("t: {}, off: {}", *t, off);
+                            println!(
+                                "map[t]: {}, map[t + off]: {}",
+                                map[(*t + off) as usize],
+                                map[*t as usize]
+                            );
+                            map[(*t + off) as usize] = map[*t as usize];
                             map[*t as usize] = 3;
+                            println!(
+                                "map[t]: {}, map[t + off]: {}",
+                                map[(*t + off) as usize],
+                                map[*t as usize]
+                            );
                         }
                     }
+                    print_map(c, &map, width);
                 }
             }
         }
     }
+    print_map(' ', &map, width);
     for (i, c) in map.iter().enumerate() {
         if c == &0usize {
             result += i as i32 / width * 100 + i as i32 % width;
         }
     }
 
-    fn walk(map:&Vec<usize>,mut to_move:Vec<i32>,cursor:i32,off:i32) -> (bool, Vec<i32>) {
-        let mut is_valid:bool;
-        
-        if map[cursor as usize] == 2 {
-            return (false,vec![]);
+    fn walk(map: &Vec<usize>, mut to_move: Vec<i32>, cursor: i32, off: i32) -> (bool, Vec<i32>) {
+        let mut is_valid: bool;
+        let test_pos = cursor + off;
+        if map[test_pos as usize] == 2 {
+            return (false, vec![]);
         }
-        if map[cursor as usize] == 3 {
+        if map[test_pos as usize] == 3 {
             to_move.push(cursor);
-            return( true, to_move);
+            return (true, to_move);
         }
-        let mut next_pos:Vec<i32> = vec![cursor + off];
+        let mut next_pos: Vec<i32> = vec![test_pos];
         if cursor == 0 {
             next_pos.push(cursor + off + 1)
         } else {
             next_pos.push(cursor + off - 1);
         }
-        let mut ret_to_move:Vec<i32> = vec![];
+        let mut ret_to_move: Vec<i32>;
         for p in &next_pos {
-            
-            (is_valid,ret_to_move) = walk(map,to_move, *p, off);
+            (is_valid, ret_to_move) = walk(map, to_move, *p, off);
             if is_valid == false {
                 return (false, vec![]);
             }
@@ -203,6 +215,19 @@ fn solve2(lines: &Vec<String>) -> i32 {
         }
         (true, to_move)
     }
- 
+
     result
+}
+
+fn print_map(dir: char, map: &Vec<usize>, width: i32) {
+    let sym = vec!['[', ']', '#', '.', '@'];
+    println!("dir: {}", dir);
+    for (i, m) in map.iter().enumerate() {
+        print!("{}", sym[*m]);
+        if i % width as usize == width as usize - 1 {
+            println!("");
+        }
+    }
+    println!("");
+    pause();
 }
